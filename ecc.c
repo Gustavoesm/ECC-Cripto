b74ae60bdf1a2f540371a175822f52872c205baf
Rafael Alonso <rafael.palonso@gmail.com>
2019-11-13 16:40:33 -0300

    It's working, but it's not right

--- ecc.c
+++ ecc.c
@@ -2,28 +2,54 @@
 #include <stdio.h>
 #include <gmp.h>
 
-unsigned long extendedAlg(mpz_t a, mpz_t b);
+unsigned long extendedAlg(int a, int b);
 
-void main(){
+int main(){
+  int n, a, p, gx, gy, rx, ry, delta;
+  unsigned long inverse;
+
   // take the first parameter (n)
+  scanf("%d", &n);
 
   // while n is not 0
+  while(n != 0){
     // take 4 integers (a, p, Gx, Gy)
+    scanf("%d %d %d %d", &a, &p, &gx, &gy);
 
     // create a result var (Rx, Ry) passing the same values as Gx and Gy
+    rx = gx;
+    ry = gy;
 
+    n--;
     // (n-1) times do
-      // if Gx == Rx && Gy == Ry
-        // set delta equal to (3Xq^2 + a) / (2Yq) mod p
-      // else
-        // set delta equal to (Yg – Yq) / (Xg-Xq) mod p
+    while (n != 0){
+      if(gx == rx && gy == ry){
+        // set delta equal to (3 * Gx^2 + a) / (2 * Gy) mod p
+        inverse = extendedAlg(p, (2 * gy));
+        delta = ((3 * (gx * gx) + a) % p) * inverse;
+        delta = delta % p;
+      } else {
+        // set delta equal to (Ry – Gy) / (Rx - Gx) mod p
+        inverse = extendedAlg(p, (rx - gx));
+        delta = ((ry - gy) % p) * inverse;
+        delta = delta % p;
+      }
 
-      // calculate the new Rx = ( delta * 2 - Xq - Xg ) mod p
-      // calculate the new Yx = ( delta * (Xq – Xr) - Yq ) mod p
+      // calculate the new Rx = ( delta * 2 - Gx - Rx ) mod p
+      rx = (delta * 2 - gx - rx) % p;
+      // calculate the new Yx = ( delta * (Gx – Rx) - Gy ) mod p
+      ry = (delta * (gx - rx) - gy) % p;
 
+      n--;
+    }
     // print the result (Rx, Ry)
+    printf("%d %d\n", rx, ry);
 
     // take the new first parameter (n)
+    scanf("%d", &n);
+  }
+
+  return 0;
 }
 
 unsigned long extendedAlg(int a, int b){
@@ -79,7 +105,7 @@ unsigned long extendedAlg(int a, int b){
   }
 
   if(mpz_cmp_ui(n, 0) < 0){
-    mpz_add(n, n, a);
+    mpz_add_ui(n, n, a);
   }
 
   mpz_clear(x);
